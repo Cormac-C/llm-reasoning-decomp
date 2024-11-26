bde8b8c728e7e371004a9e26b56fa271b2070b3d
Cormac Cureton <cormaccureton@gmail.com>
2024-11-25 23:16:45 -0500

    Remove inloop eval

--- scripts/train_baseline_zebra.py
+++ scripts/train_baseline_zebra.py
@@ -44,8 +44,7 @@ def get_sft_config(run_name=None):
         output_dir="/tmp",
         run_name=run_name,
         # Eval_strategy set to "no" temporarily cause of https://github.com/huggingface/transformers/issues/34701
-        eval_strategy="steps",
-        eval_steps=50,
+        eval_strategy="no",
         report_to="wandb",
         logging_steps=10,
         dataset_batch_size=16,
