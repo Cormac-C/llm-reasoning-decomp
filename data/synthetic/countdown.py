d889e40df4b78aac5d65d105344d5eee924455b0
abhijeetpraveen <abhijeetpraveen@gmail.com>
2024-12-01 17:06:21 -0500

    started stream of search implementation

---
+++ data/synthetic/countdown.py
@@ -0,0 +1,37 @@
+from torch.utils.data import Dataset
+import json
+
+class Countdown(Dataset):
+    def __init__(self, json_file):
+        with open(json_file, 'r') as f:
+            self.data = json.load(f)
+
+        # Filter examples with rating > 0.9
+        self.data = [item for item in self.data if item["rating"] > 0.9]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+
+        nums = item["nums"]
+        target = item["target"]
+        solution = item["solution"]
+        search_path  = item["search_path"]
+        optimal_path = item["optimal_path"]
+
+
+        question = (
+            f"The target is: {target}\n"
+            f"The available numbers are: {nums}\n"
+            f"Describe how to reach the target using the given numbers."
+        )
+
+        answer = (
+            f"The search path used for this problem was: {search_path}\n"
+            f"The optimal path was: {optimal_path}\n"
+            f"The final solution was: {solution}"
+        )
+
+        return {"question": question, "answer": answer}
