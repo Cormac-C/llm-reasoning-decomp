c0e4c0050ae9adcbe9fca246081b76c9ce2f70f2
Cormac Cureton <cormaccureton@gmail.com>
2024-12-04 16:43:40 -0500

    Try few shot with zebra and sos

--- scripts/sos_eval_zebra.py
+++ scripts/sos_eval_zebra.py
@@ -7,6 +7,7 @@ from peft import peft_model, PeftModel
 from dotenv import load_dotenv
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import Dataset
+from torch.utils.data import Subset
 
 # Setup module path for local imports
 module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -14,7 +15,11 @@ if module_path not in sys.path:
     sys.path.append(module_path)
 
 from data.zebra import Zebra
-from data.format import chat_format_qa_instance, lm_format_qa_instance
+from data.format import (
+    chat_format_qa_instance,
+    lm_format_qa_instance,
+    chat_create_fewshot_prompt,
+)
 from evals.zebra_eval import eval_model_zebra
 
 # Load environment variables
@@ -35,10 +40,24 @@ ADAPTER_DIR = "/home/mila/x/xiaoyin.chen/scratch/projects/decomp/files/sos-1b/ll
 
 MODEL_NAME = "meta-llama/Llama-3.2-1B-Instruct"
 
-def load_prep_zebra_dataset(tokenizer, instruction_tuned=True, test_split_size=0.2):
+
+def load_prep_zebra_dataset(
+    tokenizer, instruction_tuned=True, few_shot=None, test_split_size=0.2
+):
     dataset = Zebra(hf_token=os.environ["HF_TOKEN"])
     if instruction_tuned:
-        formatted_list = [chat_format_qa_instance(example) for example in dataset]
+        formatted_list = []
+        if few_shot is not None:
+            fewshot_examples = Subset(dataset, range(few_shot))
+            dataset = Subset(dataset, range(few_shot, len(dataset)))
+            formatted_list = [
+                chat_create_fewshot_prompt(
+                    example, examples=fewshot_examples, num_shots=few_shot
+                )
+                for example in dataset
+            ]
+        else:
+            formatted_list = [chat_format_qa_instance(example) for example in dataset]
         formatted_list = tokenizer.apply_chat_template(
             formatted_list, tokenize=False, add_generation_prompt=False
         )
@@ -48,6 +67,7 @@ def load_prep_zebra_dataset(tokenizer, instruction_tuned=True, test_split_size=0
 
     dataset = dataset.train_test_split(test_size=test_split_size)
     return dataset
+
 
 # Load base model and adapter
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, token=os.environ["HF_TOKEN"])
@@ -70,7 +90,7 @@ peft_model.eval()
 
 # Load dataset
 dataset = load_prep_zebra_dataset(
-    tokenizer, instruction_tuned=True, test_split_size=0.2
+    tokenizer, instruction_tuned=True, test_split_size=0.2, few_shot=3
 )
 
 dataset = dataset["test"]
